184d9a879342ab28b1653309ea8e162e8d616835
UrielAndrade <andradeleiru@gmail.com>
2025-10-04 13:55:45 -0400

    Add TypeOrmModule and Course entity to CoursesModule imports

--- src/features/courses/courses.module.ts
+++ src/features/courses/courses.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CoursesResolver } from './courses.resolver';
 import { CoursesController } from 'src/features/courses/courses.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Course } from 'src/features/courses/entities/course.entity';
 
 @Module({
+  imports: [ TypeOrmModule.forFeature([Course]) ],
   controllers: [CoursesController],
   providers: [CoursesResolver, CoursesService],
   exports: [CoursesService],
